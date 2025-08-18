564c70aed490f2d52c318a302e7b6f311aa2f34e
CompassionateITWEBDEV <mase2025ai@gmail.com>
2025-08-18 16:56:41 -0400

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,8 +4,8 @@ import type { MetadataRoute } from "next"
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: "*",
-      allow: "/",
+      userAgent: "*",    // apply to all crawlers
+      allow: "/",        // allow crawling of all paths
     },
     sitemap: "https://serenity-b9.onrender.com/sitemap.xml",
   }
