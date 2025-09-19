01041f5c0f86ab1f013fdf059172dfa8b606e813
CompassionateITWEBDEV <mase2025ai@gmail.com>
2025-09-19 08:29:51 -0400

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { Header } from "@/components/header";
+import  Header  from "@/components/header";
 import { Footer } from "@/components/footer";
 import { Button } from "@/components/ui/button";
 import {
