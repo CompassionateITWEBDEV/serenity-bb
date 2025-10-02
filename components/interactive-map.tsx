819f033e4242f9b143fc671b079b4074c3992040
CompassionateITWEBDEV <mase2025ai@gmail.com>
2025-10-02 16:46:22 -0400

    Update interactive-map.tsx

--- components/interactive-map.tsx
+++ components/interactive-map.tsx
@@ -3,36 +3,33 @@
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
 
-// Avoid SSR issues by loading react-leaflet dynamically on client
+// Load react-leaflet on client only (avoids SSR issues)
 const MapInner = dynamic(async () => {
   const L = await import("leaflet");
   const { MapContainer, TileLayer, Marker, Popup } = await import("react-leaflet");
 
-  // Fix default icon paths in Next.js
+  // Fix Leaflet marker icons in Next.js
   // @ts-ignore
   delete (L.Icon.Default as any).prototype._getIconUrl;
   L.Icon.Default.mergeOptions({
-    iconRetinaUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-    iconUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-    shadowUrl:
-      "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
   });
 
-  // Geocode once: 123 Recovery St, Pontiac, MI 48341 (approx coords)
-  const CENTER: [number, number] = [42.6389, -83.2910];
+  // Approx coords for: 123 Recovery St, Pontiac, MI 48341
+  const CENTER: [number, number] = [42.6389, -83.291];
 
-  return function Inner() {
+  function Inner() {
     return (
       <MapContainer
         center={CENTER}
         zoom={15}
         style={{ height: "100%", width: "100%", borderRadius: "0.5rem" }}
-        scrollWheelZoom={true}
+        scrollWheelZoom
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution="&copy; OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={CENTER}>
@@ -54,7 +51,9 @@ const MapInner = dynamic(async () => {
         </Marker>
       </MapContainer>
     );
-  };
+  }
+
+  return Inner;
 }, { ssr: false });
 
 export default function InteractiveMap() {
